f7eceb6ffb8c9d55a1cccaab9bbb16bc841cdb14
mawrick26 <mawrick26@gmail.com>
2018-12-27 15:28:05 +0100

    cpufreq_interactive: disable trace
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -41,8 +41,10 @@
 #include "cpu_load_metric.h"
 #endif
 
-#define CREATE_TRACE_POINTS
+//#define CREATE_TRACE_POINTS
+#ifdef CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
+#endif
 
 #define CONFIG_DYNAMIC_MODE_SUPPORT
 //#define CONFIG_DYNAMIC_MODE_SUPPORT_DEBUG
@@ -551,9 +553,11 @@ static void cpufreq_interactive_timer(unsigned long data)
 	    new_freq > pcpu->policy->cur &&
 	    now - pcpu->pol_hispeed_val_time <
 	    freq_to_above_hispeed_delay(tunables, pcpu->policy->cur)) {
+#ifdef CREATE_TRACE_POINTS
 		trace_cpufreq_interactive_notyet(
 			data, cpu_load, pcpu->target_freq,
 			pcpu->policy->cur, new_freq);
+#endif
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto target_update;
 	}
@@ -588,9 +592,11 @@ static void cpufreq_interactive_timer(unsigned long data)
 	if (new_freq < pcpu->floor_freq &&
 	    pcpu->target_freq >= pcpu->policy->cur) {
 		if (now - max_fvtime < tunables->min_sample_time) {
+#ifdef CREATE_TRACE_POINTS
 			trace_cpufreq_interactive_notyet(
 				data, cpu_load, pcpu->target_freq,
 				pcpu->policy->cur, new_freq);
+#endif
 			spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 			goto rearm;
 		}
@@ -613,16 +619,20 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	if (pcpu->target_freq == new_freq &&
 			pcpu->target_freq <= pcpu->policy->cur) {
+#ifdef CREATE_TRACE_POINTS
 		trace_cpufreq_interactive_already(
 			data, cpu_load, pcpu->target_freq,
 			pcpu->policy->cur, new_freq);
+#endif
 		spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 		goto rearm;
 	}
 
+#ifdef CREATE_TRACE_POINTS
 	trace_cpufreq_interactive_target(data, cpu_load, pcpu->target_freq,
 					 pcpu->policy->cur, new_freq);
-
+#endif
+    
 	pcpu->target_freq = new_freq;
 	spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 	spin_lock_irqsave(&speedchange_cpumask_lock, flags);
@@ -739,10 +749,11 @@ static int cpufreq_interactive_speedchange_task(void *data)
 #if defined(CONFIG_CPU_THERMAL_IPA)
 			ipa_cpufreq_requested(pcpu->policy, max_freq);
 #endif
+#ifdef CREATE_TRACE_POINTS
 			trace_cpufreq_interactive_setspeed(cpu,
 						     pcpu->target_freq,
 						     pcpu->policy->cur);
-
+#endif
 			up_read(&pcpu->enable_sem);
 		}
 	}
@@ -1186,12 +1197,16 @@ static ssize_t store_boost(struct cpufreq_interactive_tunables *tunables,
 	tunables->boost_val = val;
 
 	if (tunables->boost_val) {
+#ifdef CREATE_TRACE_POINTS
 		trace_cpufreq_interactive_boost("on");
+#endif
 		if (!tunables->boosted)
 			cpufreq_interactive_boost(tunables);
 	} else {
 		tunables->boostpulse_endtime = ktime_to_us(ktime_get());
+#ifdef CREATE_TRACE_POINTS
 		trace_cpufreq_interactive_unboost("off");
+#endif
 	}
 
 	return count;
@@ -1209,7 +1224,9 @@ static ssize_t store_boostpulse(struct cpufreq_interactive_tunables *tunables,
 
 	tunables->boostpulse_endtime = ktime_to_us(ktime_get()) +
 		tunables->boostpulse_duration_val;
+#ifdef CREATE_TRACE_POINTS
 	trace_cpufreq_interactive_boost("pulse");
+#endif
 	if (!tunables->boosted)
 		cpufreq_interactive_boost(tunables);
 	return count;
@@ -2037,12 +2054,16 @@ static ssize_t store_exynos_fb_boost_dual(
 		tunables->boost_val = val;
 
 		if (tunables->boost_val) {
+#ifdef CREATE_TRACE_POINTS
 			trace_cpufreq_interactive_boost("on");
+#endif
 			if (!tunables->boosted)
 				cpufreq_interactive_boost(tunables);
 		} else {
 			tunables->boostpulse_endtime = ktime_to_us(ktime_get());
+#ifdef CREATE_TRACE_POINTS
 			trace_cpufreq_interactive_unboost("off");
+#endif
 		}
 	}
 
@@ -2073,12 +2094,16 @@ static ssize_t store_exynos_fb_boost_quad(
 		tunables->boost_val = val;
 
 		if (tunables->boost_val) {
+#ifdef CREATE_TRACE_POINTS
 			trace_cpufreq_interactive_boost("on");
+#endif
 			if (!tunables->boosted)
 				cpufreq_interactive_boost(tunables);
 		} else {
 			tunables->boostpulse_endtime = ktime_to_us(ktime_get());
+#ifdef CREATE_TRACE_POINTS
 			trace_cpufreq_interactive_unboost("off");
+#endif
 		}
 	}
 
@@ -2110,7 +2135,9 @@ static ssize_t store_exynos_fb_boostpulse_dual(
 		tunables->boostpulse_endtime = ktime_to_us(ktime_get()) +
 					tunables->boostpulse_duration_val;
 
+#ifdef CREATE_TRACE_POINTS
 		trace_cpufreq_interactive_boost("pulse");
+#endif
 		if (!tunables->boosted)
 			cpufreq_interactive_boost(tunables);
 	}
@@ -2143,7 +2170,9 @@ static ssize_t store_exynos_fb_boostpulse_quad(
 		tunables->boostpulse_endtime = ktime_to_us(ktime_get()) +
 					tunables->boostpulse_duration_val;
 
+#ifdef CREATE_TRACE_POINTS
 		trace_cpufreq_interactive_boost("pulse");
+#endif
 		if (!tunables->boosted)
 			cpufreq_interactive_boost(tunables);
 	}
@@ -2930,9 +2959,9 @@ static int cpufreq_interactive_cluster1_min_qos_handler(struct notifier_block *b
 		ret = NOTIFY_BAD;
 		goto exit;
 	}
-
+#ifdef CREATE_TRACE_POINTS
 	trace_cpufreq_interactive_cpu_min_qos(cpu, val, pcpu->policy->cur);
-
+#endif
 	if (val < pcpu->policy->cur) {
 		tunables = pcpu->policy->governor_data;
 
@@ -2981,9 +3010,9 @@ static int cpufreq_interactive_cluster1_max_qos_handler(struct notifier_block *b
 		ret = NOTIFY_BAD;
 		goto exit;
 	}
-
+#ifdef CREATE_TRACE_POINTS
 	trace_cpufreq_interactive_cpu_max_qos(cpu, val, pcpu->policy->cur);
-
+#endif
 	if (val > pcpu->policy->cur) {
 		tunables = pcpu->policy->governor_data;
 
@@ -3028,9 +3057,9 @@ static int cpufreq_interactive_cluster0_min_qos_handler(struct notifier_block *b
 		ret = NOTIFY_BAD;
 		goto exit;
 	}
-
+#ifdef CREATE_TRACE_POINTS
 	trace_cpufreq_interactive_kfc_min_qos(0, val, pcpu->policy->cur);
-
+#endif
 	if (val < pcpu->policy->cur) {
 		tunables = pcpu->policy->governor_data;
 
@@ -3074,9 +3103,9 @@ static int cpufreq_interactive_cluster0_max_qos_handler(struct notifier_block *b
 		ret = NOTIFY_BAD;
 		goto exit;
 	}
-
+#ifdef CREATE_TRACE_POINTS
 	trace_cpufreq_interactive_kfc_max_qos(0, val, pcpu->policy->cur);
-
+#endif
 	if (val > pcpu->policy->cur) {
 		tunables = pcpu->policy->governor_data;
 
