5824767012c793503f1da1816bc3fe69415923e3
mawrick26 <mawrick26@gmail.com>
2019-01-17 22:28:11 +0100

    reduce spam & debug v2
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -728,7 +728,7 @@ int disable_nonboot_cpus(void)
 	 */
 	cpumask_clear(frozen_cpus);
 
-	pr_info("Disabling non-boot CPUs ...\n");
+	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 #if defined(CONFIG_ARM_EXYNOS_MP_CPUFREQ)
 		if (cpu == first_cpu || cpu == nonboot_cluster_first_cpu)
@@ -788,7 +788,7 @@ void __ref enable_nonboot_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	pr_info("Enabling non-boot CPUs ...\n");
+	pr_debug("Enabling non-boot CPUs ...\n");
 
 	arch_enable_nonboot_cpus_begin();
 
@@ -797,7 +797,7 @@ void __ref enable_nonboot_cpus(void)
 		error = _cpu_up(cpu, 1);
 		trace_suspend_resume(TPS("CPU_ON"), cpu, false);
 		if (!error) {
-			pr_info("CPU%d is up\n", cpu);
+			pr_debug("CPU%d is up\n", cpu);
 			cpu_device = get_cpu_device(cpu);
 			if (!cpu_device)
 				pr_err("%s: failed to get cpu%d device\n",
--- kernel/panic.c
+++ kernel/panic.c
@@ -135,7 +135,9 @@ void panic(const char *fmt, ...)
 		dump_stack();
 #endif
 
+#ifdef CONFIG_SCHED_DEBUG
 	sysrq_sched_debug_show();
+#endif
 	/*
 	 * If we have crashed and we have a crash kernel loaded let it handle
 	 * everything else.
